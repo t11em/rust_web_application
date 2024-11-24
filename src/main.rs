bc5704db118137a174e8b43abd3c75533cad2cfe
TM <thfo.wt.h.y.xxx@gmail.com>
2024-11-24 20:37:29 +0900

    page 58. https://github.com/launchbadge/sqlx/issues/3589

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,79 @@
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{extract::State, http::StatusCode, routing::get, Router};
+use sqlx::{postgres::PgConnectOptions, PgPool};
 use std::net::{Ipv4Addr, SocketAddr};
 use tokio::net::TcpListener;
+
+struct DatabaseConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+}
+
+impl From<DatabaseConfig> for PgConnectOptions {
+    fn from(cfg: DatabaseConfig) -> Self {
+        Self::new()
+            .host(&cfg.host)
+            .port(cfg.port)
+            .username(&cfg.username)
+            .password(&cfg.password)
+            .database(&cfg.database)
+    }
+}
+
+fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
+    PgPool::connect_lazy_with(cfg.into())
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
 
 async fn hello_world() -> &'static str {
     "Hello, World!"
 }
 
+async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
+    let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
+    match connection_result {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/hello", get(hello_world));
+async fn main() -> Result<()> {
+    let database_cfg = DatabaseConfig {
+        host: "localhost".into(),
+        port: 5432,
+        username: "app".into(),
+        password: "passwd".into(),
+        database: "app".into(),
+    };
+    let conn_pool = connect_database_with(database_cfg);
+
+    let app = Router::new()
+        .route("/hello", get(hello_world))
+        .route("/health_check", get(health_check))
+        .route("/health_db", get(health_check_db))
+        .with_state(conn_pool);
+
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Listening on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
+
+#[tokio::test]
+async fn health_check_words() {
+    let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
+}
+
+// #[sqlx::test]
+// async fn health_check_db_works(pool: sqlx::PgPool) {
+//     // let status_code = health_check_db(State(Pool)).await;
+//     // assert_eq!(status_code, StatusCode::OK);
+// }
